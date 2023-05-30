eeb9dd6ea8e737b0a0ee276d27f874cc66929b49
Paige Stephenson <paigestephenson29@gmail.com>
2023-05-30 08:03:38 -0600

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Paige Stephenson on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
